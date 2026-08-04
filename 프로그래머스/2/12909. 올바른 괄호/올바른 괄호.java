c94695756b94634b262cbd9060dad135632ee143
Bomin_Seo <bominseo008@gmail.com>
2026-08-04 20:36:25 +0900

    [level 2] Title: 올바른 괄호, Time: 12.71 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for(char x : s.toCharArray()){
+            if(x == '('){
+                stack.push(x);
+            }
+            else{
+                if(stack.isEmpty()){
+                    answer = false;
+                    return answer;
+                }
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty())
+            answer = false;
+        
+        
+        return answer;
+    }
+}
